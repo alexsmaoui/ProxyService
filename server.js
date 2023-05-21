519e43fef424f26c331ff5966c98d274fc081fed
alexsmaoui <48091452+alexsmaoui@users.noreply.github.com>
2023-05-21 04:47:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -178,6 +178,7 @@ const processResponse = (proxyRes, res, append) => {
 
 const onProxyReq = (proxyReq, req, res, options) => {
   proxyReq.setHeader('User-Agent', proxyReq.getHeader('proxy-override-user-agent') || DEFAULT_USERAGENT);
+  proxyReq.setHeader('authorization', "A")
   console.error(proxyReq.getHeader('authorization'))
   if (REWRITE_ACCEPT_ENCODING) {
     proxyReq.setHeader('Accept-Encoding', 'gzip');
